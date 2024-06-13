4bbdc0d61ed1772154f4f5d6a959d356c7114769
DavidSTD <166560898+DavidSTD@users.noreply.github.com>
2024-06-12 23:55:18 -0700

    Update TeniDetails.js

--- components/TeniDetails.js
+++ components/TeniDetails.js
@@ -1,18 +1,22 @@
-import { View, Text, StyleSheet } from 'react-native'
+import {StyleSheet } from 'react-native'
 
-function TeniDetails({duration, complexity, affordability}){
+
+function ArtistaDetail({informacion, datos, id, categoryIds, title, imageUrl}){
     return(
 
         <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{informacion.map}</Text>
+            <Text style={styles.detailItem}>{datos.map}</Text>
+            <Text style={styles.detailItem}>{id.map}</Text>
+            <Text style={styles.detailItem}>{categoryIds.map}</Text>
+            <Text style={styles.detailItem}>{title.map}</Text>
+            <Text style={styles.detailItem}>{imageUrl.map}</Text>
         </View>
     )
 
 }
 
-export default TeniDetails
+export default ArtistaDetail
 
 const styles = StyleSheet.create({
     details: {
