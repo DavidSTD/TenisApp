164303ba3215b60126ac98bd1f14f500241c6353
DavidSTD <166560898+DavidSTD@users.noreply.github.com>
2024-06-13 00:09:04 -0700

    Update CategoriesScreens.js

--- screens/CategoriesScreens.js
+++ screens/CategoriesScreens.js
@@ -1,31 +1,59 @@
-import { FlatList, StyleSheet } from "react-native";
-import { CATEGORIES } from "../data/dummy_data";
-import CategoryGridTile from "../components/CategoryGridTile";
+import { Image, Text, View, StyleSheet, ScrollView } from "react-native"
 
+import { ARTISTAS} from '../data/dummy_data'
+import ArtistaDetail from "../components/ArtistaDetail"
 
-function CategoriesScreen({navigation}) {
+function ArtistaDetailScree({route, navigation}) {
+    const artistaId = route.params.artistaId
 
-    function renderCategoryItem(itemData) {
-        function pressHandler() {
-            navigation.navigate('TenisOverview', {
-                categoryId: itemData.item.id
-            })
-        }
+    const selectedArtista = ARTISTAS.find((artista) => artista.id === artistaId)
 
-        return (
-            <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler}/>
-        )
-    }
+    return(
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: selectedArtista.imageUrl }} />
+            <Text style={styles.title}>{selectedArtista.title}</Text>
 
-    return (
-        <FlatList 
-            data={CATEGORIES} 
-            keyExtractor={(item) => item.id}
-            renderItem={renderCategoryItem}
-            numColumns={2}
-        />
+            <View>
+                <Text style={styles.subtitle}>Datos</Text>
+                {selectedArtista.datos.map(datos => <Text style={styles.textContent} key={datos}>{datos}</Text>)}
+                <Text style={styles.subtitle}>Información</Text>
+                {selectedArtista.informacion.map(informacion => <Text style={styles.textContent} key={informacion}>{informacion}</Text>)}
+            </View>
+        </ScrollView>
     )
+
 }
 
+export default ArtistaDetailScree
 
-export default CategoriesScreen
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 350
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center'
+    },
+    subtitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 4,
+        padding: 6,
+        marginHorizontal: 24,
+        marginVertical: 4,
+        textAlign: 'center',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1
+    },
+    textContent: {
+        fontSize: 14,
+        margin: 4,
+        marginVertical: 3,
+        marginHorizontal: 24    
+    }
+
+
+})
